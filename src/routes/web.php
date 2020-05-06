7dcbfcb973397395414497d111ddc6e1dba80056
masanori.tateno <tateno4600@gmail.com>
2020-05-06 11:07:19 +0900

    ルーティングとコントローラーとviewを作成（何かしら表示するところまで）

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
+Route::get('/student', 'StudentController@index')->name('student.index');
+//Route::get('/student/create', 'StudentController@create')->name('student.create');
+//Route::post('/student', 'StudentController@store')->name('student.store');
+//Route::get('/student/{id}', 'StudentController@show')->name('student.show');
+//Route::get('/student/{id}/edit', 'StudentController@edit')->name('student.edit');
+//Route::put('/student/{id}', 'StudentController@update')->name('student.update');
+//Route::delete('/student/{id}', 'StudentController@destory')->name('student.destory');
+
+
+//Route::resource('/student', 'StudentController');
+
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
